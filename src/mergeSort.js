20efd717d31fde243b730aec7cf861d7c39fa300
meaghanbass <meaghan.bass@gmail.com>
2020-01-30 06:06:48 -0800

    added algo

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Layout from './components/layout';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 
 class MergeSort extends Component {
     render() {
@@ -7,6 +8,7 @@ class MergeSort extends Component {
             <Layout>
                 <div>
                     <h2>Merge Sort</h2>
+                    <SortingVisualizer />
                 </div>
             </Layout>
         );
